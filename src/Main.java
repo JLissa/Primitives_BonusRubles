fd0496148842cc562b69b9d4cce1b295a6be1bf7
Julia Hlimoneko <707010@gmail.com>
2022-12-17 20:55:26 +0200

    transferring calculation logic to if

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,18 @@ import com.sun.source.util.SourcePositions;
 
 public class Main {
     public static void main(String[] args) {
-        int startingScore = 100;
+        int startingScore = 500;
         int addingAmount = 1100;
         int amountRubleBonus = addingAmount / 100;
 
-        int totalScore = startingScore + addingAmount;
-        int totalScoreBonus = startingScore + addingAmount + amountRubleBonus;
+        int totalScore;
+        int totalScoreBonus;
 
         if (addingAmount >= 1000) {
+            totalScoreBonus = startingScore + addingAmount + amountRubleBonus;
             System.out.println(totalScoreBonus);
         } else {
+            totalScore = startingScore + addingAmount;
             System.out.println(totalScore);
         }
     }
